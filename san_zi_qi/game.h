69cf6806d5bb028c72085d8d65e78bf99aabeead
happybigfrog <1604110489@qq.com>
2021-03-08 13:43:39 +0800

    测试

---
+++ san_zi_qi/game.h
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#define ROW 3
+#define COL 3
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void initboard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void playermove(char board[ROW][COL], int row, int col);
+void computermove(char board[ROW][COL], int row, int col);
+char IsWin(char board[ROW][COL], int row, int col);
---
+++ san_zi_qi/text.c
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "game.h"
+
+void menu()
+{
+	printf("*****************************\n");
+	printf("***   1.play     0.exit   ***\n");
+	printf("*****************************\n");
+}
+
+void game()
+{
+	char ret = 0;
+	//存放整个棋盘信息
+	char board[ROW][COL] = { 0 };
+	//初始化棋盘
+	initboard(board, ROW, COL);
+	//打印棋盘
+	DisplayBoard(board, ROW, COL);
+	//下棋
+	while (1)
+	{
+		//玩家下棋
+		playermove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断玩家是否赢
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		//电脑下棋
+		computermove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+	}
+	if (ret == '*')
+	{
+		printf("玩家赢\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢\n");
+	}
+	else
+	{
+		printf("平局\n");
+	}
+}
+
+void text()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择：>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("输入错误，请重新输入\n");
+			break;
+		}
+	} while (input);
+}
+
+int main()
+{
+	text();
+	return 0;
+}
