39fcd17923fd9649e5e30276019cb56c5b728608
happybigfrog <1604110489@qq.com>
2021-03-29 23:41:57 +0800

    快捷键

---
+++ text_3_29/text.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdlib.h>
+
+int main()
+{
+	//打开计算器	//notepad记事本	//mspaint画图板
+	//system("calc");
+	//system可以打开一个外部应用程序，如果带路径使用\\或/
+	//system("C:/Users/Administrator/Desktop/geek卸载工具（自打开）/geek卸载工具（自打开）.exe");
+	//快捷键
+	//alt+↑\↓交换这一行代码位置
+	//alt+→自动补全
+	//ctrl+shift+u将小写转换为大写
+	//ctrl+u将大写转换为小写
+}
